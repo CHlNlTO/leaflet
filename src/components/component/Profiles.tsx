b85dfa117be0be8366e3b471cc1b661bf7003a56
Chinito <clark.wayne023@gmail.com>
2024-05-20 20:29:20 +0800

    Fix Item Type on Profiles

--- src/components/component/Profiles.tsx
+++ src/components/component/Profiles.tsx
@@ -2,11 +2,21 @@
 import React from "react";
 import { AnimatedTooltip } from "../ui/animated-tooltip";
 import { people } from "@/lib/const";
+import { StaticImageData } from "next/image";
+
+const convertImageToString = (image: string | StaticImageData): string => {
+  return typeof image === "string" ? image : image.src;
+};
+
+const normalizedPeople = people.map((person) => ({
+  ...person,
+  image: convertImageToString(person.image),
+}));
 
 export default function Profiles() {
   return (
     <div className="flex flex-row items-center justify-center mb-10 w-full">
-      <AnimatedTooltip items={people} />
+      <AnimatedTooltip items={normalizedPeople} />
     </div>
   );
 }
